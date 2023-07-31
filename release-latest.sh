a10e70c159fccedc08d713f3a1885975d524b0ec
eko5624 <562433403@qq.com>
2023-07-31 23:12:46 +0800

    Update release-latest.sh

--- release-latest.sh
+++ release-latest.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -ex
-CURL_RETRIES="--connect-timeout 60 --retry 5 --retry-delay 5"
+CURL_RETRIES="--connect-timeout 60 --retry 5 --retry-delay 5 --http1.1"
 
 # Delete assets
 asset_id=($(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/latest \
-| jq -r '."assets"[] | select(.name | startswith("'"$1"'")) | .id'))
+| jq -r '.assets[] | select(.name | startswith("'"$1"'")) | .id' | tr -d '\r'))
 
 for id in "${asset_id[@]}"; do
   curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
