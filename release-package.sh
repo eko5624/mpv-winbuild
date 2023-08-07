db2a9e3e159707144a998e6f3d3a4556f3504299
eko5624 <562433403@qq.com>
2023-08-07 13:05:52 +0800

    Update release-package.sh

--- release-package.sh
+++ release-package.sh
@@ -1,35 +1,26 @@
 #!/bin/bash
-set -ex
-CURL_RETRIES="--connect-timeout 60 --retry 5 --retry-delay 5"
+set -ex  
+CURL_RETRIES="--connect-timeout 60 --retry 5 --retry-delay 5 --http1.1"
 
-# Delete assets
-asset_id=($(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
-  -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/toolchain \
-| jq -r '.assets[] | select(.name | startswith("'"$1"'")) | .id'))
-
-for id in "${asset_id[@]}"; do
-  curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
-    -X DELETE \
-    -H "Accept: application/vnd.github.v3+json" \
-    https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/assets/$id;
-done    
+#Get mpv latest commit sha
+short_sha=$(cat /github/home/opt/mpv/SHORT_SHA)
 
 # Release assets
+date=$(date +%Y-%m-%d)
+
 curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases \
-  -d '{"tag_name": "toolchain"}'
+  -d '{"tag_name":"'"$date"'","name":"'"$date"'","body":"Bump to mpv-player/mpv@'"$short_sha"'"}'
   
 release_id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/toolchain | jq -r '.id')
+  https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/$date | jq -r '.id')
   
-for f in *.7z; do 
+for f in *.7z; do
   curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
     -X POST -H "Accept: application/vnd.github.v3+json" \
     -H "Content-Type: $(file -b --mime-type $f)" \
-    https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/$release_id/assets?name=$(basename $f) \
-    --data-binary @$f; 
+    https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/$release_id/assets?name=$(basename $f) --data-binary @$f; 
 done
