a84cd519b2ae3e3ea0448eb047289926346c46cc
eko5624 <562433403@qq.com>
2023-10-31 17:20:41 +0800

    Update release-package.sh

--- release-package.sh
+++ release-package.sh
@@ -4,15 +4,17 @@ CURL_RETRIES="--connect-timeout 60 --retry 5 --retry-delay 5"
 
 #Get mpv latest commit sha
 short_sha=$(cat /github/home/opt/mpv/SHORT_SHA)
-
-# Release assets
-date=$(date +%Y-%m-%d)
+body=$(cat <<END
+Bump to mpv-player/mpv@${sha}
+**Compiler**: clang
+END
+)
 
 curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases \
-  -d '{"tag_name":"'"$date"'","name":"'"$date"'","body":"Bump to mpv-player/mpv@'"$short_sha"'"}'
+  -d '{"tag_name":"'"$date"'","name":"'"$date"'","body":"'"${body}"'"}'
   
 release_id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -H "Accept: application/vnd.github.v3+json" \
