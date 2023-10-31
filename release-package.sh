493cfc678f44ce6c0dd913f925f20ea9d42fa32e
eko5624 <562433403@qq.com>
2023-10-31 19:28:10 +0800

    Update release-package.sh

--- release-package.sh
+++ release-package.sh
@@ -10,19 +10,22 @@ Bump to mpv-player/mpv@${short_sha}
 END
 )
 
-curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
+id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/releases \
-  -d '{"tag_name":"'"$date"'","name":"'"$date"'","body": "$body"}'
-  
-release_id=$(curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
-  -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/$date | jq -r '.id')
+  -d @- <<END | jq -r '.id' 
+{
+  "tag_name": "'"$date"'",
+  "name":"'"$date"'",
+  "body": "$(echo ${body//$'\n'/'\n'})"
+}
+END
+)
   
 for f in *.7z; do
   curl -u $GITHUB_ACTOR:$GH_TOKEN $CURL_RETRIES \
     -X POST -H "Accept: application/vnd.github.v3+json" \
     -H "Content-Type: $(file -b --mime-type $f)" \
-    https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/$release_id/assets?name=$(basename $f) --data-binary @$f; 
+    https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/$id/assets?name=$(basename $f) --data-binary @$f; 
 done
