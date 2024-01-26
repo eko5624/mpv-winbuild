3405ee7814897ba76e0232d8735dc906b15348ea
eko5624 <562433403@qq.com>
2024-01-26 17:58:12 +0800

    misc

--- autoupdate.py
+++ autoupdate.py
@@ -77,33 +77,3 @@ for p in pkgs:
       if l.startswith('pkgver'):
         l = 'pkgver=%s\n' % pkgs[p]
       f.write(l)
-pkgs['amf-headers-dev'] = x['amf']
-pkgs['angle-headers-dev'] = x['angle']
-pkgs['libsixel-dev'] = x['libsixel']     
-pkgs['luajit2-dev'] = x['luajit2']
-pkgs['mujs-dev'] = x['mujs']
-pkgs['rubberband-dev'] = x['rubberband']
-pkgs['vapoursynth-dev'] = x['VapourSynth'][1:]
-pkgs['ffmpeg-dev'] = x['ffmpeg']
-pkgs['ffmpeg-git'] = x['ffmpeg']
-pkgs['libmpv-git'] = x['mpv']
-pkgs['mpv-git'] = x['mpv']
-
-for t in ['build-all.yml', 'libplacebo.yml', 'ffmpeg.yml', 'mpv.yml']:
-  with in_place.InPlace('.github/workflows/%s' % t, newline='') as f:
-    for l in f:
-      if (i:=l.find('/dev/')) > -1:
-        r = l.find('-1-x86_64')
-        rr = l.rfind('-', i, r)
-        p = l[i+5:rr]
-        if p in pkgs:
-          l = '%s%s-%s%s' % (l[:i+5], p, pkgs[p], l[r:])
-      elif (i:=l.find('/latest/')) > -1:
-        r = l.find('-1-x86_64')
-        rr = l.rfind('-', i, r)
-        p = l[i+8:rr]
-        if p in pkgs:
-          l = '%s%s-%s%s' % (l[:i+8], p, pkgs[p], l[r:])
-      elif (i:=l.find('/yt-dlp/releases/download/')) > -1:
-        l = '%s%s/yt-dlp.exe\n' % (l[:i+26], x['yt-dlp'])                 
-      f.write(l)      
