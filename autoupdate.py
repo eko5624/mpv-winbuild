bc629e627d473d5d79798d46481c4c690c6e3c8e
eko5624 <562433403@qq.com>
2024-01-28 18:39:34 +0800

    Update autoupdate.py

--- autoupdate.py
+++ autoupdate.py
@@ -77,3 +77,45 @@ for p in pkgs:
       if l.startswith('pkgver'):
         l = 'pkgver=%s\n' % pkgs[p]
       f.write(l)
+pkgs['amf-headers-dev'] = x['amf']
+pkgs['angle-headers-dev'] = x['angle']
+pkgs['libsixel-dev'] = x['libsixel']     
+pkgs['luajit2-dev'] = x['luajit2']
+pkgs['mujs-dev'] = x['mujs']
+pkgs['rubberband-dev'] = x['rubberband']
+pkgs['vapoursynth-dev'] = x['VapourSynth'][1:]
+pkgs['ffmpeg-dev'] = x['ffmpeg']
+pkgs['ffmpeg-git'] = x['ffmpeg']
+pkgs['libmpv-git'] = x['mpv']
+pkgs['mpv-git'] = x['mpv']
+
+for t in ['build-mpv-clang.yml', 'build-mpv-gcc.yml', 'ffmpeg.yml', 'mpv.yml', 'libplacebo.yml', 'shaderc.yml', 'vulkan.yml']:
+  with in_place.InPlace('.github/workflows/%s' % t, newline='') as f:
+    for l in f:
+      if (i:=l.find('/dev-llvm-clang-$BIT/')) > -1:
+        r = l.find('-1-x86_64')
+        rr = l.rfind('-', i, r)
+        p = l[i+21:rr]
+        if p in pkgs:
+          l = '%s%s-%s%s' % (l[:i+21], p, pkgs[p], l[r:])
+      elif (i:=l.find('/latest-llvm-clang-$BIT/')) > -1:
+        r = l.find('-1-x86_64')
+        rr = l.rfind('-', i, r)
+        p = l[i+24:rr]
+        if p in pkgs:
+          l = '%s%s-%s%s' % (l[:i+24], p, pkgs[p], l[r:])
+      elif (i:=l.find('/dev-gcc-mcf/')) > -1:
+        r = l.find('-1-x86_64')
+        rr = l.rfind('-', i, r)
+        p = l[i+13:rr]
+        if p in pkgs:
+          l = '%s%s-%s%s' % (l[:i+13], p, pkgs[p], l[r:])
+      elif (i:=l.find('/latest-gcc-mcf/')) > -1:
+        r = l.find('-1-x86_64')
+        rr = l.rfind('-', i, r)
+        p = l[i+16:rr]
+        if p in pkgs:
+          l = '%s%s-%s%s' % (l[:i+16], p, pkgs[p], l[r:])
+      elif (i:=l.find('/yt-dlp/releases/download/')) > -1:
+        l = '%s%s/yt-dlp.exe\n' % (l[:i+26], x['yt-dlp'])                 
+      f.write(l)
