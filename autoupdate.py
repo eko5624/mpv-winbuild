d1bc6b38b54c969933c922a4ef8c4c49c08084f3
eko5624 <562433403@qq.com>
2024-05-27 22:44:00 +0800

    remove some *.yml

--- autoupdate.py
+++ autoupdate.py
@@ -96,7 +96,7 @@ pkgs['mpv-git'] = x['mpv']
 pkgs['mpv-thinlto'] = x['mpv']
 pkgs['shaderc-thinlto'] = x['shaderc']
 
-for t in ['build-mpv.yml', 'build-mpv-gcc.yml', 'ffmpeg.yml', 'mpv.yml', 'libplacebo.yml', 'shaderc.yml', 'vulkan.yml']:
+for t in ['build-mpv.yml', 'ffmpeg.yml', 'mpv.yml', 'libplacebo.yml', 'shaderc.yml', 'vulkan.yml']:
   with in_place.InPlace('.github/workflows/%s' % t, newline='') as f:
     for l in f:
       if (i:=l.find('/dev-$COMPILER-$BIT/')) > -1:
